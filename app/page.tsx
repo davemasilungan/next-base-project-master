787ddc169fbd09c249d3bf4acd656bb9f5cc0504
dhipflp <MASILUNGANDA@innovasi.com>
2023-11-20 10:43:08 -0800

    fixed css

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex flex-col items-center pt-28">
       <div className="items-center font-mono text-sm">
-        <p className="fixed top-0 flex justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+        <p className="top-0 static flex w-auto p-4 justify-center bg-gray-200 border border-gray-300 rounded-xl">
           Get started by editing&nbsp;
           <code className="font-bold">app/page.tsx</code>
         </p>
